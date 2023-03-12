c7f3b2d6fee5920a0133d6c8e37b3e41d06af370
Anton <61093173+ghtx280@users.noreply.github.com>
2023-03-12 15:31:27 +0200

    0.0.1

---
+++ grabIcons.js
@@ -0,0 +1,34 @@
+function grabIcons(url) {
+  if (!url.startsWith('http')) {
+    url = "https://" + url
+  }
+  return new Promise((res)=>{
+    let allorigins = `https://api.allorigins.win/raw?url=${encodeURIComponent(url)}`
+
+    fetch(allorigins)
+    .then((response) => {
+      if (response.ok) {
+        return response.text()
+      }
+      throw new Error("Error getting icons");
+    })
+    .then((data) => {
+      let elem = document.createElement("html")
+      let loc  = document.createElement("a")
+      loc.href = url
+      elem.innerHTML = data;
+      res(
+        Array
+        .from(elem.querySelectorAll("[rel*='icon']"))
+        .map(e => {
+          let href = e.getAttribute('href')
+          return href.startsWith('/') 
+          ? loc.origin + href
+          : loc.origin + (
+            loc.pathname.endsWith('/') ? loc.pathname : (loc.pathname + '/')
+          ) + href
+        })
+      )
+    });
+  })
+}
